2efbddf97bc418ea5efa306f6d2e179fee4ca8f1
admin <admin@admins-MacBook-Pro.local>
2022-01-27 15:18:35 -0800

    updated styling choices for NavBar and also added burger menu option and js code to activate

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -6,69 +6,74 @@ import "bulma/css/bulma.min.css";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Main = () => {
+  function toggleBurgerMenu() {
+    document.querySelector(".navbar-menu").classList.toggle("is-active");
+  }
   return (
     <div>
-      <article class="media">
-        <figure class="media-left">
-          <a class="image is-96x96" href="/">
-            <img
-              src={bcover}
-              width="65"
-              height="100"
-              alt="Sensory Series Logo"
-            />
-          </a>
-        </figure>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-menu is-active">
-            <div id="navbarBasicExample" class="navbar-menu is-flex-mobile">
-              <div class="navbar-start">
-                <Link to={"/creators"} class="navbar-item is-tab">
-                  <button class="button is-primary">Creators</button>
-                </Link>
-                <Link to={"/books"} class="navbar-item is-tab">
-                  <button class="button is-primary">Books</button>
-                </Link>
-                <Link to={"/resources"} class="navbar-item is-tab">
-                  <button class="button is-primary">Resources</button>
-                </Link>
-                <Link to={"/blogs"} class="navbar-item is-tab">
-                  <button class="button is-primary">Blog</button>
-                </Link>
-                <Link to={"/contact"} class="navbar-item is-tab">
-                  <button class="button is-primary">Contact Us!</button>
-                </Link>
-                {/* <Link to={"/jessicakeenan/reviews"} class="navbar-item">
-            <button class="button is-primary">Therapy Reviews</button>
-          </Link> */}
-              </div>
-              <a
+      <div class="columns is-narrow-mobile">
+        <div class="column is-1">
+          <figure class="media-left">
+            <a class="image is-96x96" href="/">
+              <img
+                src={bcover}
+                width="65"
+                height="100"
+                alt="Sensory Series Logo"
+              />
+            </a>
+          </figure>
+        </div>
+        <div class="column">
+          <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+              <div
                 role="button"
                 class="navbar-burger"
                 aria-label="menu"
-                href
                 aria-expanded="false"
-                data-target="navbarBasicExample"
+                data-target="Options"
+                onClick={toggleBurgerMenu}
+                href
               >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div>
-          </div>
-
-          {/* <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="icon is-large">
-                <FontAwesomeIcon icon={["fab", "facebook"]} />
-                <a href="https://www.linkedin.com/in/jessica-keenan-918937a3/">
-                  <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            <div id="Options" class="navbar-menu">
+              <div class="navbar-start">
+                <a href={"/creators"} class="navbar-item is-tab">
+                  <button class="button is-primary">Creators</button>
+                </a>
+                <a href={"/books"} class="navbar-item is-tab">
+                  <button class="button is-primary">Books</button>
+                </a>
+                <a href={"/resources"} class="navbar-item is-tab">
+                  <button class="button is-primary">Resources</button>
+                </a>
+                <a href={"/blogs"} class="navbar-item is-tab">
+                  <button class="button is-primary">Blog</button>
+                </a>
+                <a href={"/contact"} class="navbar-item is-tab">
+                  <button class="button is-primary">Contact Us!</button>
                 </a>
               </div>
             </div>
-          </div> */}
-        </nav>
-      </article>
+
+            {/* <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="icon is-large">
+                  <FontAwesomeIcon icon={["fab", "facebook"]} />
+                  <a href="https://www.linkedin.com/in/jessica-keenan-918937a3/">
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                  </a>
+                </div>
+              </div>
+            </div> */}
+          </nav>
+        </div>
+      </div>
 
       <div class="columns is-multiline is-mobile is-vcentered">
         <div class="column is-half is-offset-one-quarter">
